b78b78ad281d9843542feb8e58bfec9723ded0da
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-06-06 13:21:26 -0400

    animations

--- src/pages/home/components/what-we-offer-card.js
+++ src/pages/home/components/what-we-offer-card.js
@@ -1,14 +1,17 @@
 import { Component } from 'react'
 import { Image } from 'react-bootstrap'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class WhatWeOfferCard extends Component {
     render() {
         return (
-            <div className='what-we-offer-card'>
-                <Image src={this.props.icon} alt="icon"/>
-                <h5 className='mt-1 mb-3'>{this.props.title}</h5>
-                <p className='text-muted'>{this.props.content}</p>
-            </div>
+            <ScrollAnimation animateIn='rubberBand' animateOnce>
+                <div className='what-we-offer-card'>
+                    <Image src={this.props.icon} alt="icon" />
+                    <h5 className='mt-1 mb-3'>{this.props.title}</h5>
+                    <p className='text-muted'>{this.props.content}</p>
+                </div>
+            </ScrollAnimation>
         )
     }
 }
