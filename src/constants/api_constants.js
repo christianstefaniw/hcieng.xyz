fba261cdd2ff331d03a668ffc517f78eadabce7a
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-06-09 08:50:21 -0400

    server endpoint fix

--- src/constants/api_constants.js
+++ src/constants/api_constants.js
@@ -1,5 +1,5 @@
 import config from '../config'
 
 
-export const base = config.DEBUG ? 'http://localhost:8080' : 'https://hcieng-server.herokuapp.com/';
+export const base = config.DEBUG ? 'http://localhost:8080' : 'https://hcieng-server.herokuapp.com';
 export const email_endpoint = `${base}/email`;
