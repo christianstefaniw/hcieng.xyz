077b8b9e6fca1154f668b3326b1b9d11e2d3adf1
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-06-08 23:36:57 -0400

    seo

--- readme.md
+++
@@ -1,3 +0,0 @@
-<div align="center">
-    <h1>👷 HCI Engineering Club Frontend 👷</h1>
-</div>
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import { Helmet } from "react-helmet";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,16 +14,23 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
+      <>
+        <Helmet>
+          <title>HCI Eng</title>
+          <meta name="description" content="Humberside's official engineering club! Join now!" />
+          <meta name="keywords" content="hci, humberside, engineering club, engineering, humberside collegiate institute" />
+        </Helmet>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
+      </>
     )
   }
 }
