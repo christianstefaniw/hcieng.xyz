608ab3728f5a096fc813e31f2f6236e3e6eb2366
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-06-18 18:41:30 -0400

    ws https

--- src/constants/api_constants.js
+++ src/constants/api_constants.js
@@ -12,5 +12,5 @@ export const standard_login_endpoint = `${base}/${auth}/login`
 export const oauth_login_endpoint = `${base}/${auth}/login/google`
 export const account_info_endpoint = `${base}/${account}/info`
 export const all_user_rooms = `${base}/${chat}/allrooms`
-export const websocket = `ws://${domain_name}/chat/room`
+export const websocket = `${config.DEBUG ? 'ws': 'wss'}://${domain_name}/chat/room`
 export const room_info = `${base}/${chat}/info`
