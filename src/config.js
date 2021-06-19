4c31825841bb1f08d37cae98dd4cbb9bad804cb9
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-06-18 20:06:35 -0400

    mobile friendly chat

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     DEBUG: false,
     HCI_ENG_ROOM_ID: '60cbe63c7b127cb2a206eeaf',
     ANNOUNCEMENTS_ROOM_ID: '60cbe61fb06d3f760cd6a4a6',
-    MSG_LOAD_INTERVAL: 100
+    MSG_LOAD_INTERVAL: 101
 }
 
 export default config
