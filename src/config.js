96841e7fcf144c9cc6030f7bb625aa2afbc9593e
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-06-20 13:51:48 -0400

    debug false

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    DEBUG: true,
+    DEBUG: false,
     HCI_ENG_ROOM_ID: '60cbe63c7b127cb2a206eeaf',
     ANNOUNCEMENTS_ROOM_ID: '60cbe61fb06d3f760cd6a4a6',
     MSG_LOAD_INTERVAL: 101
